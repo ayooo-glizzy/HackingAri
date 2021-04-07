e00cbea8246966d75a69823bdfea25d1f33b20b0
ayooo-glizzy <68714224+ayooo-glizzy@users.noreply.github.com>
2021-04-07 14:52:53 -0400

    Add files via upload

--- HackingAri.py
+++ HackingAri.py
@@ -50,3 +50,48 @@ screen_clear()
 print(logo + Fore.CYAN + "initialize password cracker...")
 sleep(1)
 screen_clear()
+print(logo + Fore.CYAN + "Loading Ncrack \ ")
+sleep(1)
+screen_clear()
+print(logo + Fore.CYAN + "Loading Ncrack | ")
+sleep(1)
+screen_clear()
+print(logo + Fore.CYAN + "Loading Ncrack / ")
+sleep(1)
+screen_clear()
+print(logo + Fore.CYAN + "Loading Ncrack - ")
+sleep(1)
+screen_clear()
+print(logo + Fore.CYAN + "Loading Ncrack \ ")
+sleep(1)
+screen_clear()
+print(logo + Fore.CYAN + "Loading Ncrack | ")
+sleep(1)
+screen_clear()
+print(logo + Fore.CYAN + "Loading Ncrack / ")
+sleep(1)
+screen_clear()
+print(logo + Fore.CYAN + "Loading Ncrack - ")
+sleep(1)
+screen_clear()
+print(logo + Fore.CYAN + "Loading Ncrack \ ")
+sleep(3)
+screen_clear()
+def menu():
+    print (logo + """
+   {1} ~ Discord
+   {2} ~ Instagram
+   {3} ~ Facebook
+   {0} ~ Exit
+ """)
+    choice = raw_input("SomeFakeShit~# ")
+    if choice == "1":
+        Dis()
+    elif choice == "0":
+        clearScr(), sys.exit()
+    elif choice == "":
+        menu()
+    else:
+        menu()
+def Dis():
+    print("Checking Arianas Password Right Now")
