22b3cf25f9f43b5d8183f767327610d4a03dae0a
ayooo-glizzy <68714224+ayooo-glizzy@users.noreply.github.com>
2021-04-08 03:33:32 -0400

    Add files via upload

--- HackingAri.py
+++ HackingAri.py
@@ -4,6 +4,11 @@ colorama.init(autoreset=True)
 from time import sleep
 import time
 import os
+import random, string
+rp = random.SystemRandom()
+length = 10
+alphabet = string.ascii_letters + string.digits
+password = str().join(rp.choice(alphabet) for _ in range(length))
 # The screen clear function
 def screen_clear():
    # for mac and linux(here, os.name is 'posix')
@@ -12,6 +17,28 @@ def screen_clear():
    else:
       # for windows platfrom
       _ = os.system('cls')
+
+yes = set(['yes', 'y', 'ye', 'Y'])
+no = set(['no', 'n', 'N', 'NO'])
+
+uplogo = Fore.RED + """
+            ██╗   ██╗██████╗ ██████╗  █████╗ ████████╗███████╗
+            ██║   ██║██╔══██╗██╔══██╗██╔══██╗╚══██╔══╝██╔════╝
+            ██║   ██║██████╔╝██║  ██║███████║   ██║   █████╗  
+            ██║   ██║██╔═══╝ ██║  ██║██╔══██║   ██║   ██╔══╝  
+            ╚██████╔╝██║     ██████╔╝██║  ██║   ██║   ███████╗
+             ╚═════╝ ╚═╝     ╚═════╝ ╚═╝  ╚═╝   ╚═╝   ╚══════╝
+"""
+
+instalogo = Fore.RED + """
+            ██╗███╗   ██╗███████╗████████╗ █████╗  ██████╗ ██████╗  █████╗ ███╗   ███╗
+            ██║████╗  ██║██╔════╝╚══██╔══╝██╔══██╗██╔════╝ ██╔══██╗██╔══██╗████╗ ████║
+            ██║██╔██╗ ██║███████╗   ██║   ███████║██║  ███╗██████╔╝███████║██╔████╔██║
+            ██║██║╚██╗██║╚════██║   ██║   ██╔══██║██║   ██║██╔══██╗██╔══██║██║╚██╔╝██║
+            ██║██║ ╚████║███████║   ██║   ██║  ██║╚██████╔╝██║  ██║██║  ██║██║ ╚═╝ ██║
+            ╚═╝╚═╝  ╚═══╝╚══════╝   ╚═╝   ╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝     ╚═╝
+"""
+
 
 
 logo = Fore.RED + """    
@@ -37,6 +64,16 @@ wrong = Fore.RED + """
                                      ║  ├─┤│ ││ │└─┐├┤   ╠═╣││││ │ │ ├─┤├┤ ├┬┘
                                      ╚═╝┴ ┴└─┘└─┘└─┘└─┘  ╩ ╩┘└┘└─┘ ┴ ┴ ┴└─┘┴└
 """
+
+dislogo = Fore.RED + """ 
+            ██████╗ ██╗███████╗ ██████╗ ██████╗ ██████╗ ██████╗ 
+            ██╔══██╗██║██╔════╝██╔════╝██╔═══██╗██╔══██╗██╔══██╗
+            ██║  ██║██║███████╗██║     ██║   ██║██████╔╝██║  ██║
+            ██║  ██║██║╚════██║██║     ██║   ██║██╔══██╗██║  ██║
+            ██████╔╝██║███████║╚██████╗╚██████╔╝██║  ██║██████╔╝
+            ╚═════╝ ╚═╝╚══════╝ ╚═════╝ ╚═════╝ ╚═╝  ╚═╝╚═════╝ 
+"""
+
 
 
 screen_clear()
@@ -104,23 +141,237 @@ def error():
 
 def Dis():
     screen_clear()
-    print (Fore.GREEN + "Checking for Password.")   
+    cool = "@gmail.com"
+    global choicedis
+    choicedis = input(dislogo + Fore.CYAN + "Please Enter Email: ")
+    if cool in choicedis:
+       print(Fore.GREEN + "Email Valid Proceeding")
+       sleep(2)
+       Dis2()
+    else:
+        screen_clear()
+        print(dislogo + Fore.RED + "invalid Email Retard, Try Again")
+        sleep(2)
+        Dis()
+        
+def Dis2():
+    screen_clear()
+    print (dislogo + Fore.LIGHTYELLOW_EX + "Checking for Accounts associated with: " + choicedis + ".")
+    sleep(2)
+    screen_clear()
+    print (dislogo + Fore.LIGHTYELLOW_EX + "Checking for Accounts associated with: " + choicedis + "..")
+    sleep(2)
+    screen_clear()
+    print (dislogo + Fore.LIGHTYELLOW_EX + "Checking for Accounts associated with: " + choicedis + "...")
+    sleep(2)
+    screen_clear()
+    print (dislogo + Fore.LIGHTYELLOW_EX + "Checking for Accounts associated with: " + choicedis + ".")
+    sleep(2)
+    screen_clear()
+    print (dislogo + Fore.LIGHTYELLOW_EX + "Checking for Accounts associated with: " + choicedis + "..")
+    sleep(2)
+    screen_clear()
+    print (dislogo + Fore.LIGHTYELLOW_EX + "Checking for Accounts associated with: " + choicedis + "...")
+    sleep(2)
+    screen_clear()
+    print (dislogo + Fore.GREEN + "Account Found, Account is: ursoftgirlfriend#8035")
+    sleep(3)
+    screen_clear()
+    print (dislogo + Fore.LIGHTYELLOW_EX + "Checking for Password.")
+    sleep(2)
+    screen_clear()
+    print (dislogo + Fore.LIGHTYELLOW_EX + "Checking for Password..")
+    sleep(2)
+    screen_clear()
+    print (dislogo + Fore.LIGHTYELLOW_EX + "Checking for Password...")
+    sleep(2)
+    screen_clear()
+    print (dislogo + Fore.LIGHTYELLOW_EX + "Checking for Password.")
+    sleep(2)
+    screen_clear()
+    print (dislogo + Fore.LIGHTYELLOW_EX + "Checking for Password..")
+    sleep(2)
+    screen_clear()
+    print (dislogo + Fore.LIGHTYELLOW_EX + "Checking for Password...")
+    sleep(2)
+    screen_clear()
+    print (dislogo + Fore.GREEN + "Done password successfully cracked")
+    sleep(1)
+    screen_clear()
+    print (dislogo + Fore.GREEN + "Password is: Matthew0101")
+    sleep(3)
+    menu()
+
+def insta():
+    screen_clear()
+    cool = "nigga"
+    cool2 = "faggot"
+    cool3 = "beaner"
+    cool4 = "wetback"
+    cool5 = "spic"
+    cool6 = "Nigga"
+    cool7 = "Faggot"
+    cool8 = "Beaner"
+    cool9 = "Wetback"
+    cool10 = "Spic"
+    global choice3
+    print(instalogo)
+    choice3 = input(Fore.CYAN + "Please Enter IG Username: ")
+    if cool in choice3:
+        screen_clear()
+        print(instalogo + Fore.RED + "This user name contains profanity. Please try another!")
+        sleep(3)
+        screen_clear()
+        insta()
+    if cool2 in choice3:
+        screen_clear()
+        print(instalogo + Fore.RED + "This user name contains profanity. Please try another!")
+        sleep(3)
+        screen_clear()
+        insta()
+    if cool3 in choice3:
+        screen_clear()
+        print(instalogo + Fore.RED + "This user name contains profanity. Please try another!")
+        sleep(3)
+        screen_clear()
+        insta()
+    if cool4 in choice3:
+        screen_clear()
+        print(instalogo + Fore.RED + "This user name contains profanity. Please try another!")
+        sleep(3)
+        screen_clear()
+        insta()
+    if cool5 in choice3:
+        screen_clear()
+        print(instalogo + Fore.RED + "This user name contains profanity. Please try another!")
+        sleep(3)
+        screen_clear()
+        insta()
+    if cool6 in choice3:
+        screen_clear()
+        print(instalogo + Fore.RED + "This user name contains profanity. Please try another!")
+        sleep(3)
+        screen_clear()
+        insta()
+    if cool7 in choice3:
+        screen_clear()
+        print(instalogo + Fore.RED + "This user name contains profanity. Please try another!")
+        sleep(3)
+        screen_clear()
+        insta()
+    if cool8 in choice3:
+        screen_clear()
+        print(instalogo + Fore.RED + "This user name contains profanity. Please try another!")
+        sleep(3)
+        screen_clear()
+        insta()
+    if cool9 in choice3:
+        screen_clear()
+        print(instalogo + Fore.RED + "This user name contains profanity. Please try another!")
+        sleep(3)
+        screen_clear()
+        insta()
+    if cool10 in choice3:
+        screen_clear()
+        print(instalogo + Fore.RED + "This user name contains profanity. Please try another!")
+        sleep(3)
+        screen_clear()
+        insta()
+    else:
+        screen_clear()
+        print(instalogo + Fore.GREEN + "This user is valid. Proceeding")
+        sleep(3)
+        screen_clear()
+        insta2()
+
+def insta2():
+    screen_clear()
+    print(instalogo + Fore.LIGHTYELLOW_EX + "Checking for instagram accounts with the name: " + choice3 + ".")
+    sleep(2)
+    screen_clear()
+    print(instalogo + Fore.LIGHTYELLOW_EX + "Checking for instagram accounts with the name: " + choice3 + "..")
+    sleep(2)
+    screen_clear()
+    print(instalogo + Fore.LIGHTYELLOW_EX + "Checking for instagram accounts with the name: " + choice3 + "...")
+    sleep(2)
+    screen_clear()
+    print(instalogo + Fore.LIGHTYELLOW_EX + "Checking for instagram accounts with the name: " + choice3 + ".")
+    sleep(2)
+    screen_clear()
+    print(instalogo + Fore.LIGHTYELLOW_EX + "Checking for instagram accounts with the name: " + choice3 + "..")
+    sleep(2)
+    screen_clear()
+    print(instalogo + Fore.LIGHTYELLOW_EX + "Checking for instagram accounts with the name: " + choice3 + "...")
+    sleep(2)
+    screen_clear()
+    print(instalogo + Fore.GREEN + "Instagram account with the name: " + choice3 + " Found!")
+    sleep(2)
+    screen_clear()
+    print(instalogo + Fore.LIGHTYELLOW_EX + "Checking for passwords.")
+    sleep(2)
+    screen_clear()
+    print(instalogo + Fore.LIGHTYELLOW_EX + "Checking for passwords..")
+    sleep(2)
+    screen_clear()
+    print(instalogo + Fore.LIGHTYELLOW_EX + "Checking for passwords...")
+    sleep(2)
+    screen_clear()
+    print(instalogo + Fore.LIGHTYELLOW_EX + "Checking for passwords.")
+    sleep(2)
+    screen_clear()
+    print(instalogo + Fore.LIGHTYELLOW_EX + "Checking for passwords..")
+    sleep(2)
+    screen_clear()
+    print(instalogo + Fore.LIGHTYELLOW_EX + "Checking for passwords...")
+    sleep(2)
+    screen_clear()
+    print(instalogo + Fore.GREEN + "Password found: " + password)
+    sleep(2)
+    screen_clear()
+    print(instalogo + Fore.GREEN + "Username: " + choice3 + """
+Password: """ + password)
+    sleep(4)
+    menu()
+
+
+def update():
+    screen_clear()
+    print (uplogo + Fore.LIGHTYELLOW_EX + "This Tool is Only Available for Termux and Similar Systems. ")
+    choiceupdate = input("Continue Y / N: ")
+    if choiceupdate in yes:
+        os.system("cd Desktop")
+        os.system("rm -rf HackingAri")
+        os.system("git clone https://github.com/ayooo-glizzy/HackingAri.git")
+        os.system("cd Exploit-Tools && sudo bash ./update.sh")
+        os.system("HackingAri")
+    else:
+        screen_clear()
+        print(uplogo + Fore.RED + "The Password Cracker was not updated")
+        sleep(2)
+        menu()
 
 
 def menu():
+    screen_clear()
     print (logo + """
    {1} ~ Discord
    {2} ~ Instagram
    {3} ~ Facebook
+   {4} ~ Update Password Cracker
    {0} ~ Exit
  """)
     choice = input(Fore.CYAN + "PassHaxor~# ")
     if choice == "1":
         Dis()
     elif choice == "0":
-        clearScr(), sys.exit()
+        screen_clear(), exit()
+    elif choice == "2":
+        insta()
+    elif choice == "4":
+        update()
     elif choice == "":
         menu()
     else:
         error()
 menu()
+
